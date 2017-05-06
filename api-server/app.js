e96f00fb08acca4b2392dc4d52ef9c7d09c2e25f
Alberto <Alberto>
2017-05-06 12:35:08 +0200

    arreglo de mou

--- api-server/app.js
+++ api-server/app.js
@@ -5,11 +5,7 @@
 //Importamos express de otro
 var api = require('./routes/ejercicio'); //carga todo lo que haya en el fichero de rutas
 var api2= require('./routes/actividad');
-<<<<<<< HEAD
-var api3= require('./routes/diccionario');
-=======
 var apiProfesor= require('./routes/profesor');
->>>>>>> d9402544b3624ed31f9438c3fe684b0fe04257ee
 
 var bodyParser= require('body-parser'); //Esto es un midelware que se carga antes de nuestro script
 var express= require('express'); 
@@ -33,12 +29,7 @@ app.use((req, res, next) =>{
 });
 
 app.use('/api', api);//Lo cargamo dentro de express. El primer parametro es el prefijo que se va a usar para todas las rutas. El segundo es el objeto qyue se va a cargar: api
-<<<<<<< HEAD
-app.use('/api', api2);
-app.use('/api', api3);//diccionario
-=======
 app.use('/api2', api2);
 app.use('/apiProfesor', apiProfesor);
->>>>>>> d9402544b3624ed31f9438c3fe684b0fe04257ee
 //Con esto permitimos que este modulo sea importado con require en otros modulos
 module.exports= app;
