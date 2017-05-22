cac0397eea467b35535940b2d724d96724234d9b
Alberto <Alberto>
2017-05-22 23:17:13 +0200

    Datos de admin y conexion segura a mongo

--- client-web/usuarios-mongo.js
+++ client-web/usuarios-mongo.js
@@ -1,27 +1,12 @@
 var user1= {
-    "_id" : ObjectId("5916f32a7fe47c0988825f28"),
-    "role" : "profesor",
-    "institucion_educativa" : "Complutense",
+    "role" : "admin",
+    "institucion_educativa" : "UCM",
     "fecha_nacimiento" : ISODate("1980-06-03T22:00:00.000Z"),
-    "email" : "antonio@ucm.es",
-    "dni" : "xxx",
+    "email" : "",
+    "dni" : "",
     "password" : "1234",
     "apellidos" : "Sarasa",
     "nombre" : "Antonio",
-    "alias" : "antonio",
-    "__v" : 0
-}
-
-var user2= {
-    "_id" : ObjectId("5916fd0033384923445869f7"),
-    "role" : "alumno",
-    "institucion_educativa" : "Complutense",
-    "fecha_nacimiento" : ISODate("1988-03-10T23:00:00.000Z"),
-    "email" : "adaimiel@ucm.es",
-    "dni" : "47287251X",
-    "password" : "1234",
-    "apellidos" : "Daimiel Blanco",
-    "nombre" : "Alberto",
-    "alias" : "berti",
+    "alias" : "admin",
     "__v" : 0
 }
