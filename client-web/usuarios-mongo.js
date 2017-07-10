68046b4c8b0fff5a79ca43157f51f61b43960351
Alberto <Alberto>
2017-07-10 18:11:21 +0200

    user

--- client-web/usuarios-mongo.js
+++ client-web/usuarios-mongo.js
@@ -1,12 +1,15 @@
+//Usuario admin
+var userpass= "admin/1234";
 var user1= {
+    "_id" : ObjectId("5960994b0eb35513807e7df3"),
     "role" : "admin",
     "institucion_educativa" : "UCM",
-    "fecha_nacimiento" : ISODate("1980-06-03T22:00:00.000Z"),
-    "email" : "",
-    "dni" : "",
-    "password" : "1234",
-    "apellidos" : "Sarasa",
-    "nombre" : "Antonio",
+    "fecha_nacimiento" : ISODate("1988-11-03T00:00:00.000Z"),
+    "email" : "bertilo_@hotmail.com",
+    "dni" : "47287251X",
+    "password" : "$2a$10$N82E.pana4T44xScY9J/COKcmo7yftR.1s/EbCRorBlqZqCurmMyi",
+    "apellidos" : "Daimiel Blanco",
+    "nombre" : "Alberto",
     "alias" : "admin",
     "__v" : 0
 }
