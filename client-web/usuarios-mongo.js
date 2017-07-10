e2bf3c479c138045f9cc73db7c44dd6f16d4290a
Alberto <Alberto>
2017-07-10 18:12:35 +0200

    cambios en info user

--- client-web/usuarios-mongo.js
+++ client-web/usuarios-mongo.js
@@ -5,11 +5,11 @@ var user1= {
     "role" : "admin",
     "institucion_educativa" : "UCM",
     "fecha_nacimiento" : ISODate("1988-11-03T00:00:00.000Z"),
-    "email" : "bertilo_@hotmail.com",
-    "dni" : "47287251X",
+    "email" : "prueba@ucm.com",
+    "dni" : "57842136X",
     "password" : "$2a$10$N82E.pana4T44xScY9J/COKcmo7yftR.1s/EbCRorBlqZqCurmMyi",
-    "apellidos" : "Daimiel Blanco",
-    "nombre" : "Alberto",
+    "apellidos" : "",
+    "nombre" : "Administrador",
     "alias" : "admin",
     "__v" : 0
 }
